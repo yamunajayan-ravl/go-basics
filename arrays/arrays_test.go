020bede5d80d13ba437dd68bc62c3a00f9baf7c6
Yamuna Jayan <yamunajayan@yamunas-air.home>
2026-03-09 09:36:40 -0400

    add arrays

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,15 +1,64 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"golang.org/x/exp/slices"
+)
 
 func TestArrays(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+	
 }
+func TestSumAll(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{3, 4, 5})
+		want := []int{0, 12}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+// func TestSumAll(t *testing.T) {
+
+// 	got := SumAll([]int{1, 2}, []int{0, 9})
+// 	want := []int{3, 9}
+
+// 	if !slices.Equal(got, want) {
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+// }
+
+
+
+	
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my-project
 
 go 1.25.0
+
+require golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa // indirect
